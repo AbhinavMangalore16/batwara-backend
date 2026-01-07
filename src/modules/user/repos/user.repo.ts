88621ea218173afe59a81b4276e61495dbb56be6
MOONCHILD2403 <yashasbajaj2403@gmail.com>
2026-01-08 01:59:21 +0530

    cypher randi

--- src/modules/user/repos/user.repo.ts
+++ src/modules/user/repos/user.repo.ts
@@ -1,4 +1,5 @@
 import { db } from '../../../shared/infra/db/postgres/postgres-client.config.js'; // Import your DB client
+import { graph } from '../../../shared/infra/db/neo4j/neo4j-client.config.js
 import type { dtoTypes } from '../dtos/index';
 import { Schemas } from '../dtos/index';
 import { user } from './auth.schema.js';
@@ -14,6 +15,31 @@ export class UserPGRepository {
   }
 
   async updateUserDetail(id:string,patchUserObject:dtoTypes["PatchUserDTO"]){
+    const res = await db.
+    update(user).
+    set(Schemas.PatchUserSchema.parse(patchUserObject)).
+    where(eq(user.id,id));
+    return null;
+  }
+
+  async addFriend(id:string,friendId:string){
+    const driver = graph();
+    let { records, summary } = await driver.executeQuery(`
+						MATCH (p:Person)-[r:FRIENDS_WITH]-(n:Person)
+            FILTER p.id:$userId,n.id:$friendId`,
+						{ userId:id,
+              friendId:friendId
+             }
+					)
+    if(!records){
+      await driver.executeQuery(`
+						CREATE (p:Person)-[r:FRIENDS_WITH]-(n:Person)
+            FILTER p.id:$userId,n.id:$friendId`,
+						{ userId:id,
+              friendId:friendId
+             }
+					)
+    }
     const res = await db.
     update(user).
     set(Schemas.PatchUserSchema.parse(patchUserObject)).
